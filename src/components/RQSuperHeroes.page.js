1922d013a730708c429e7044779e8055d4530dd7
yhorman perez <jhppunk@gmail.com>
2022-04-21 14:41:13 -0500

    implementing use query on demand

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -5,7 +5,7 @@ const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 function RQSuperHeroes() {
-  const { data, isLoading, isError, error } = useQuery(
+  const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
@@ -13,8 +13,9 @@ function RQSuperHeroes() {
         // staleTime: 30000, // it will keep the data cached and marked as fresh for 30 seconds, avoiding refetching
         // refetchOnMount: true, //refetch data and updates the ui on every mounting
         // refetchOnWindowFocus: true, //refetch data and updates the ui everytime the window receives the focus
-        refetchInterval: 5000, //refetch data and updates the ui every 5 seconds, only if window has focus
-        refetchIntervalInBackground: true //refetch data and updates the ui every 5 seconds even if th window loses focus
+        // refetchInterval: 5000, //refetch data and updates the ui every 5 seconds, only if window has focus
+        // refetchIntervalInBackground: true //refetch data and updates the ui every 5 seconds even if th window loses focus
+        enabled: false
     }
   );
 
@@ -23,12 +24,13 @@ function RQSuperHeroes() {
     return <h2>{error}</h2>;
   }
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero.name}> {hero.name} </div>;
       })}
