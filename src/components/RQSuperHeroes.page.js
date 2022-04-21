bef6f88d9d71ef646a1754563b8527b8295e7797
yhorman perez <jhppunk@gmail.com>
2022-04-20 23:31:03 -0500

    implementation of react query error handling

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -5,12 +5,19 @@ const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 function RQSuperHeroes() {
-  const { data, isLoading } = useQuery("super-heroes", fetchSuperHeroes);
+  const { data, isLoading, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes
+  );
+
+
+  if (isError) {
+    return <h2>{error}</h2>;
+  }
 
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
-
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,29 +1,40 @@
-import axios from 'axios'
-import React, { useState , useEffect} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function Superheroes() {
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState([])
-  
-    useEffect(() => {
-      axios.get('http://localhost:4000/superheroes').then(res => {
-        setData(res.data)
-        setIsLoading(false)
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
       })
-    }, [])
-  
-    if (isLoading) {
-      return <h2>Loading...</h2>
-    }
-  
-    return (
-      <>
-        <h2>Super Heroes Page</h2>
-        {data.map(hero => {
-          return <div>{hero.name}</div>
-        })}
-      </>
-    )
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (error) {
+    return <h2>{error}</h2>;
+  }
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
 }
 
-export default Superheroes
+export default Superheroes;
