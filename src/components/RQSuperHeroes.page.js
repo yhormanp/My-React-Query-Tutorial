715bd67cfce117c80aedce116d56c616a47ef0c9
yhorman perez <jhppunk@gmail.com>
2022-04-21 14:27:59 -0500

    different polling properties addes and commented

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -9,10 +9,12 @@ function RQSuperHeroes() {
     "super-heroes",
     fetchSuperHeroes,
     {
-        // cacheTime: 5000,
-        // staleTime: 30000
-        refetchOnMount: true,
-        refetchOnWindowFocus: true
+        // cacheTime: 5000, //it will cache the data for 5 seconds
+        // staleTime: 30000, // it will keep the data cached and marked as fresh for 30 seconds, avoiding refetching
+        // refetchOnMount: true, //refetch data and updates the ui on every mounting
+        // refetchOnWindowFocus: true, //refetch data and updates the ui everytime the window receives the focus
+        refetchInterval: 5000, //refetch data and updates the ui every 5 seconds, only if window has focus
+        refetchIntervalInBackground: true //refetch data and updates the ui every 5 seconds even if th window loses focus
     }
   );
 
