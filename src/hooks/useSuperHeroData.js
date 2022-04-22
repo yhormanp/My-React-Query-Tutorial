28fe6e52f16422a0e256c45e75eee657135114c3
yhorman perez <jhppunk@gmail.com>
2022-04-22 17:34:38 -0500

    setting initial query data to the super hero details

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchSuperHero = (heroId) => {
@@ -6,5 +6,21 @@ const fetchSuperHero = (heroId) => {
 };
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(["super-hero", heroId], fetchSuperHero(heroId));
+  const queryClient = useQueryClient();
+  return useQuery(["super-hero", heroId], () => fetchSuperHero(heroId), {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData("super-heroes")
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+
+      if (hero) {
+        return {
+          //   data: hero,
+          data: { name: hero.name },
+        };
+      } else {
+        return undefined;
+      }
+    },
+  });
 };
